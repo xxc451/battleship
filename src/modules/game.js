d64deb8106dc9ada0c51a5fca12e0802e37f84f1
Xiaoyi Chen <126677643+xxc451@users.noreply.github.com>
2026-02-21 19:00:40 +0100

    Add random ship placements and game buttons

--- src/modules/game.js
+++ src/modules/game.js
@@ -2,9 +2,12 @@ import Player from "./player.js";
 
 let human;
 let computer;
+let gameStart;
+const shipLengths = [5, 4, 3, 3, 2];
 
 const dialog = document.querySelector("dialog");
 const dialogClose = document.querySelector("dialog button");
+const gameButtonDiv = document.querySelector(".game-buttons");
 
 dialog.addEventListener("close", () => {
     initGame();
@@ -21,6 +24,90 @@ function createElementWithClass(element, className) {
     return node;
 }
 
+function randomizeShipPlacement(player) {
+    for (const length of shipLengths) {
+        let placed = false;
+        while (!placed) {
+            const randX = Math.floor(Math.random() * 10);
+            const randY = Math.floor(Math.random() * 10);
+            const horizontal = Math.random() < 0.5;
+
+            let nextToShip = false;
+            const boardLength = player.board.board.length;
+
+            for (let k = -1; k <= length; k++) {
+                for (let l = -1; l <= 1; l++) {
+                    const x = horizontal ? randX + l : randX + k;
+                    const y = horizontal ? randY + k : randY + l;
+
+                    if (
+                        x >= 0 &&
+                        x < boardLength &&
+                        y >= 0 &&
+                        y < boardLength
+                    ) {
+                        if (player.board.board[x][y].ship) {
+                            nextToShip = true;
+                            break;
+                        }
+                    }
+                }
+                if (nextToShip) {
+                    break;
+                }
+            }
+            if (!nextToShip) {
+                placed = player.board.placeShip(
+                    randX,
+                    randY,
+                    length,
+                    horizontal,
+                );
+            }
+        }
+    }
+}
+
+function createRandomizeButton() {
+    const button = createElementWithClass("button", "random secondary");
+    button.textContent = "Randomize Board";
+
+    button.addEventListener("click", () => {
+        if (!gameStart) {
+            human = new Player(true);
+            randomizeShipPlacement(human);
+            renderBoards();
+        }
+    });
+
+    return button;
+}
+
+function createResetButton() {
+    const button = createElementWithClass("button", "reset secondary");
+    button.textContent = "Reset Game";
+
+    button.addEventListener("click", () => {
+        initGame();
+        renderBoards();
+    });
+
+    return button;
+}
+
+function createStartButton() {
+    const button = createElementWithClass("button", "start primary");
+    button.textContent = "Start Game";
+
+    button.addEventListener("click", () => {
+        gameStart = true;
+        gameButtonDiv.textContent = "";
+        gameButtonDiv.append(createResetButton());
+    });
+
+    return button;
+}
+
 function displayWinner(isHuman) {
     const header = document.querySelector("dialog h1");
 
@@ -32,11 +119,11 @@ function displayWinner(isHuman) {
 function initGame() {
     human = new Player(true);
     computer = new Player(false);
-    human.board.placeShip(0, 0, 5, true);
-    human.board.placeShip(1, 0, 3, true);
-
-    computer.board.placeShip(0, 0, 5, true);
-    computer.board.placeShip(1, 0, 3, true);
+    gameStart = false;
+    randomizeShipPlacement(human);
+    randomizeShipPlacement(computer);
+    gameButtonDiv.textContent = "";
+    gameButtonDiv.append(createRandomizeButton(), createStartButton());
 }
 
 function createBoard(player) {
@@ -70,29 +157,31 @@ function renderBoards() {
     const computerBoard = createBoard(computer);
 
     computerBoard.addEventListener("click", (e) => {
-        if (e.target.classList.contains("cell")) {
-            const x = parseInt(e.target.dataset.x);
-            const y = parseInt(e.target.dataset.y);
-            if (computer.board.receiveAttack(x, y)) {
-                if (computer.board.allShipSunk()) {
-                    renderBoards();
-                    displayWinner(true);
-                    return;
-                }
-                // computer turn
-                let computerHit = false;
-                while (!computerHit) {
-                    const computerX = Math.floor(Math.random() * 10);
-                    const computerY = Math.floor(Math.random() * 10);
-                    computerHit = human.board.receiveAttack(
-                        computerX,
-                        computerY,
-                    );
-                }
+        if (gameStart) {
+            if (e.target.classList.contains("cell")) {
+                const x = parseInt(e.target.dataset.x);
+                const y = parseInt(e.target.dataset.y);
+                if (computer.board.receiveAttack(x, y)) {
+                    if (computer.board.allShipSunk()) {
+                        renderBoards();
+                        displayWinner(true);
+                        return;
+                    }
+                    // computer turn
+                    let computerHit = false;
+                    while (!computerHit) {
+                        const computerX = Math.floor(Math.random() * 10);
+                        const computerY = Math.floor(Math.random() * 10);
+                        computerHit = human.board.receiveAttack(
+                            computerX,
+                            computerY,
+                        );
+                    }
 
-                renderBoards();
-                if (human.board.allShipSunk()) {
-                    displayWinner(false);
+                    renderBoards();
+                    if (human.board.allShipSunk()) {
+                        displayWinner(false);
+                    }
                 }
             }
         }
